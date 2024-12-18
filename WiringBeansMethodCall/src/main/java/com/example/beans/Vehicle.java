adb8a77b621b0311c12610b5b435cfff3b8657b5
Chinmay Sai <yanamalachinmaysai106@gmail.com>
2024-12-18 08:40:33 +0530

    Committing Wiring beans using method call changes.

--- WiringBeansMethodCall/src/main/java/com/example/beans/Vehicle.java
+++ WiringBeansMethodCall/src/main/java/com/example/beans/Vehicle.java
@@ -11,7 +11,6 @@ public class Vehicle {
     public void setName(String name) {
         this.name = name;
     }
-
     public void printHello()
     {
         System.out.println("Vehicle Component");
