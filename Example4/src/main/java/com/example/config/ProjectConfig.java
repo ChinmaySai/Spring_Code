a4b23c1efe633f525a3519b3d8d0678315e3f5af
Chinmay Sai <yanamalachinmaysai106@gmail.com>
2024-12-18 08:53:21 +0530

    Committing Example 4 Changes

---
+++ Example4/src/main/java/com/example/config/ProjectConfig.java
@@ -0,0 +1,33 @@
+package com.example.config;
+
+import com.example.beans.Vehicle;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class ProjectConfig {
+
+    @Bean(name="Audi")
+    Vehicle vehicle1()
+    {
+        var veh=new Vehicle();
+        veh.setName("Audi");
+        return veh;
+    }
+    @Bean(value="Ferrari")
+    Vehicle vehicle2()
+    {
+        var veh=new Vehicle();
+        veh.setName("Ferrari");
+        return veh;
+    }
+    @Primary
+    @Bean("BMW")
+    Vehicle vehicle3()
+    {
+        var veh=new Vehicle();
+        veh.setName("BMW");
+        return veh;
+    }
+}
