426d253fa520116df16bf2c285e963796d9b2e2c
Chinmay Sai <yanamalachinmaysai106@gmail.com>
2025-01-17 22:20:02 +0530

    SpringBoot Starter Web Application Project with Default Configurations.

---
+++ Example_24/src/main/java/com/chinmay/georgeschool/GeorgeSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.chinmay.georgeschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeorgeSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeorgeSchoolApplication.class, args);
+	}
+
+}
