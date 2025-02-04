0193fc362faeefcb4f34b7f7faee773132f275cd
Chinmay Sai <yanamalachinmaysai106@gmail.com>
2025-02-04 17:03:23 +0530

    George School About us Changes

--- Example_30_George_School_Web_Application/src/main/java/com/eazybytes/eazyschool/EazyschoolApplication.java
+++ Example_30_George_School_Web_Application/src/main/java/com/eazybytes/eazyschool/EazyschoolApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EazyschoolApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(EazyschoolApplication.class, args);
 	}
-
 }
