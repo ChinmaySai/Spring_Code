e596a47c05c8f039092fe2a55c60e25ce0865823
Chinmay Sai <yanamalachinmaysai106@gmail.com>
2025-02-05 22:11:04 +0530

    Taking the Contact Details from the Users and displaying on the Console.

--- Example_30_George_School_Web_Application/src/main/java/com/eazybytes/eazyschool/controller/ContactController.java
+++ Example_30_George_School_Web_Application/src/main/java/com/eazybytes/eazyschool/controller/ContactController.java
@@ -1,14 +1,37 @@
 package com.eazybytes.eazyschool.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
+import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class ContactController {
+	
+	private Logger log=LoggerFactory.getLogger(ContactController.class);
 
 	@RequestMapping(value={"/contact"})
 	public String displayContact()
 	{
 		return "contact.html";
+	}
+	
+	//@RequestMapping(value= {"/saveMsg"},method = POST)
+	@PostMapping(value="/saveMsg")
+	public ModelAndView saveMessage(@RequestParam String name,
+			@RequestParam String mobileNum,@RequestParam String email,
+			@RequestParam String subject,@RequestParam String message)
+	
+	{
+		log.info("name : "+name);
+		log.info("mobileNum : "+mobileNum);
+		log.info("email : "+email);
+		log.info("subject :"+subject);
+		log.info("message : "+message);
+	return new ModelAndView("redirect:/contact");	
 	}
 }
