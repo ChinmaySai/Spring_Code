f4bb00cd9e23ba5d9ae6bc26d78ee54a599d69f5
Chinmay Sai <yanamalachinmaysai106@gmail.com>
2025-02-15 08:31:05 +0530

    @Slf4j Annotation for Creating Log Object

--- Example_30_George_School_Web_Application/src/main/java/com/eazybytes/eazyschool/service/ContactService.java
+++ Example_30_George_School_Web_Application/src/main/java/com/eazybytes/eazyschool/service/ContactService.java
@@ -1,13 +1,17 @@
 package com.eazybytes.eazyschool.service;
 import com.eazybytes.eazyschool.model.Contact;
+
+import lombok.extern.slf4j.Slf4j;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class ContactService {
 
-	private Logger log=LoggerFactory.getLogger(ContactService.class);
+	//private Logger log=LoggerFactory.getLogger(ContactService.class);
 	public boolean saveMessage(Contact contact) {
 
 		boolean isSaved=true;
