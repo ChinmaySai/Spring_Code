8db1872fca8e9c0b445bc9cc73c4a84401b5bf7d
Chinmay Sai <yanamalachinmaysai106@gmail.com>
2025-06-30 08:35:02 +0530

    Request Scope Changes Commit.

---
+++ Example_36_Request_Scope/src/main/java/com/george/georgeschool/service/ContactService.java
@@ -0,0 +1,40 @@
+package com.george.georgeschool.service;
+import lombok.extern.slf4j.Slf4j;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.RequestScope;
+
+import com.george.georgeschool.model.Contact;
+
+@Slf4j
+@Service
+@RequestScope
+public class ContactService {
+	
+	private int counter=0;
+	
+	public ContactService()
+	{
+		System.out.println("ContactService Bean Intialized");
+	}
+
+	//private Logger log=LoggerFactory.getLogger(ContactService.class);
+	public boolean saveMessage(Contact contact) {
+
+		boolean isSaved=true;
+		log.info(contact.toString());
+		return isSaved;
+	}
+	public int getCounter()
+	{
+		return counter;
+	}
+	public void setCounter(int counter)
+	{
+		this.counter=counter;
+	}
+
+	
+}
