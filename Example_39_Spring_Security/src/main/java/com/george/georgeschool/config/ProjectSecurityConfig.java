d3abc9d40ba44dc39b2dc9859730ed7115e31585
Chinmay Sai <yanamalachinmaysai106@gmail.com>
2025-07-26 11:04:52 +0530

    denyAll() configuration

--- Example_39_Spring_Security/src/main/java/com/george/georgeschool/config/ProjectSecurityConfig.java
+++ Example_39_Spring_Security/src/main/java/com/george/georgeschool/config/ProjectSecurityConfig.java
@@ -13,9 +13,15 @@ public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+    	
         http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
+        /*
+    	http.authorizeHttpRequests((requests) -> requests.anyRequest().denyAll());
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();*/
     }
 }
